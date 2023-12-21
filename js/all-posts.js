28fc2adfd2297e81cc47a0d80710802be36fa9a8
Shohislom1112 <ravshanovshohislom11@gmail.com>
2023-12-21 08:41:16 +0500

    Hali tugamadi

--- js/all-posts.js
+++ js/all-posts.js
@@ -1,39 +1,83 @@
-const postDiv = document.querySelector(".posts");
+// const postDiv = document.querySelector(".posts");
 
-async function getPostsAPI() {
+// async function getPostsAPI() {
+//   try {
+//     let res = await fetch(`http://localhost:3000/posts`);
+//     let data = await res.json();
+//     pushPosts(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// getPostsAPI();
+
+// function pushPosts(users) {
+//   let str = "";
+
+//   users.map((user) => {
+//     str += `
+//               <a href="./blog-post.html" onclick="getPost(${user.id})" class="post">
+//                 <div class="card">
+//                 <div class="post_img">
+//                   <img src="${user.image}" alt="">
+//                 </div>
+//                 <div class="post_text">
+//                   <span>${user.category}</span>
+//                   <h2>${user.title}</h2>
+//                   <p>${user.description}</p>
+//                   <h3>${user.author}</h3>
+//                 </div>
+//                 </div>
+//               </a>
+//           `;
+//   });
+//   postDiv.innerHTML = str;
+// }
+
+const postCard = document.querySelector(".cards");
+const infosList = document
+  .getElementById("cards")
+  .getElementsByClassName("post");
+
+async function getPosts() {
   try {
-    let res = await fetch(`http://localhost:3000/posts`);
-    let data = await res.json();
-    pushPosts(data);
+    let rest = await fetch("http://localhost:3000/posts");
+    let date = await rest.json();
+    pushPosts(date);
   } catch (error) {
     console.log(error);
   }
 }
 
-getPostsAPI();
+getPosts();
 
-function pushPosts(users) {
-  let str = "";
+function pushPosts(user) {
+  let Str = "";
 
-  users.map((user) => {
-    str += `
-              <a href="./blog-post.html" onclick="getPost(${user.id})" class="post">
-                <div class="card">
-                <div class="post_img">
-                  <img src="${user.image}" alt="">
-                </div>
-                <div class="post_text">
-                  <span>${user.category}</span>
-                  <h2>${user.title}</h2>
-                  <p>${user.description}</p>
-                  <h3>${user.author}</h3>
-                </div>
-                </div>
-              </a>
-          `;
+  user.map((u) => {
+    Str += `
+            <a href="./blog-post.html" onclick="getPost(${u.id})" class="post">
+              <div class="post_img">
+                <img src="${u.image}" alt="">
+              </div>
+              <div class="texts">
+                <p class="category">${u.category}</p>
+                <h2>${u.title}</h2>
+                <p>${u.description}</p>
+                <p class="job">${u.author}</p>
+              </div>
+            </a>
+        `;
   });
-  postDiv.innerHTML = str;
+  postCard.innerHTML = Str;
 }
+
+function getPost(id) {
+  localStorage.setItem("userId", id);
+  console.log(id);
+
+
 
 // search /////////////////////////////////////////////////////////
 const searchInput = document.getElementById("search");
--- js/blog-post.js
+++ js/blog-post.js
@@ -1,37 +1,80 @@
-const blog = document.querySelector(".cards");
+// const blog = document.querySelector(".cards");
 
-const userId = localStorage.getItem("userId");
-async function fitchBlock() {
-  let id = JSON.parse(localStorage.getItem("id"));
+// const userId = localStorage.getItem("userId");
+// async function fitchBlock() {
+//   let id = JSON.parse(localStorage.getItem("id"));
+//   try {
+//     let res = await fetch("http://localhost:3000/posts");
+//     let data = await res.json();
+//     let filteredData = data.slice(data.length - 1);
+//     displayPosts(filteredData);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// fitchBlock();
+
+// console.log(userId);
+
+// function displayPosts(post) {
+//   let str = "";
+
+//   post.map((post) => {
+//     str += `
+//             <div class="card">
+//               <div class="img">
+//               <img src="${post.image}" alt=""></div>
+//               <div class="text">
+//               <span>${post.author}</span>
+//               <h1>${post.title}</h1>
+//               <p>${post.description}Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.</p>
+//               <p>${post.description}Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.</p>
+//               </div>
+//             </div>
+//         `;
+//   });
+//   blog.innerHTML = str;
+// }
+
+const postCard = document.querySelector(".cards");
+const infosList = document
+  .getElementById("cards")
+  .getElementsByClassName("post");
+
+async function getPosts() {
   try {
-    let res = await fetch("http://localhost:3000/posts");
-    let data = await res.json();
-    let filteredData = data.slice(data.length - 1);
-    displayPosts(filteredData);
+    let rest = await fetch("http://localhost:3000/posts");
+    let date = await rest.json();
+    pushPosts(date);
   } catch (error) {
     console.log(error);
   }
 }
-fitchBlock();
 
-console.log(userId);
+getPosts();
 
-function displayPosts(post) {
-  let str = "";
+function pushPosts(user) {
+  let Str = "";
 
-  post.map((post) => {
-    str += `
-            <div class="card">
+  user.map((u) => {
+    Str += `
+            <a href="./blog.html" onclick="getPost(${u.id})" class="card">
               <div class="img">
-              <img src="${post.image}" alt=""></div>
-              <div class="text">
-              <span>${post.author}</span>
-              <h1>${post.title}</h1>
-              <p>${post.description}Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.</p>
-              <p>${post.description}Scelerisque viverra mauris in aliquam sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit gravida rutrum quisque non.</p>
+                <img src="${u.image}" alt="">
               </div>
-            </div>
+              <div class="text">
+                <p class="category">${u.category}</p>
+                <h2>${u.title}</h2>
+                <p>${u.description}</p>
+                <p class="job">${u.author}</p>
+              </div>
+            </a>
         `;
   });
-  blog.innerHTML = str;
+  postCard.innerHTML = Str;
+}
+
+function getPost(id) {
+  localStorage.setItem("userId", id);
+  console.log(id);
 }
